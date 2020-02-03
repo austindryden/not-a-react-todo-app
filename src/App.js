56a23122803e29812d0bcade75977930ed9f5584
Austin Dryden <austinisnorobot@gmail.com>
2020-02-03 14:48:45 -0500

    adds features

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      items: []
     };
   }
   render() {
@@ -18,6 +19,11 @@ class App extends React.Component {
             placeholder="Type something here!"
           />
         </form>
+        <div>
+          <ul>
+              {this.state.items.map((words, index)=>(<li>{words}</li>))}
+          </ul>
+          </div>
       </div>
     );
   }
@@ -25,13 +31,20 @@ class App extends React.Component {
   _handleSubmit = (event) => {
     event.preventDefault();
     console.log('You just submitted');
+    let newArr = this.state.items;
+    newArr.push(this.state.text);
+    this.setState({
+      items:newArr,
+      text:''
+    })
+    console.log(this.state.items);
   }
 
   _updateText = (event) => {
     console.log(event.target.value);
-    // this.setState({
-    //   text: event.target.value
-    // })
+    this.setState({
+      text: event.target.value
+    })
   }
 }
 
